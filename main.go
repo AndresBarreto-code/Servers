4cff2114c59bcec3d3dfb606a609aebbb9b97829
Andres Barreto <andres.barreto@koombea.com>
2021-10-12 19:00:08 -0500

    server with workers

--- main.go
+++ main.go
@@ -3,26 +3,98 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"time"
 )
 
-type Writer struct{}
+type Job struct {
+	Name   string
+	Server string
+}
 
-func (Writer) Write(p []byte) (int, error) {
-	fmt.Println(string(p))
-	return len(p), nil
+type Worker struct {
+	Id         int
+	JobQueue   chan Job
+	WorkerPool chan chan Job
+	QuitChan   chan bool
+}
+
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	MaxWorkers int
+	JobQueue   chan Job
+}
+
+func NewWorker(id int, workerPool chan chan Job) *Worker {
+	return &Worker{
+		Id:         id,
+		JobQueue:   make(chan Job),
+		WorkerPool: workerPool,
+		QuitChan:   make(chan bool),
+	}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobQueue
+			select {
+			case job := <-w.JobQueue:
+				fmt.Printf("Worker whit id %d started.\n", w.Id)
+				message := checkServer(job.Server)
+				fmt.Printf("Worker whit id %d finished. Result: %s. Name: %s.\n", w.Id, message, job.Name)
+			case <-w.QuitChan:
+				fmt.Printf("Worker whit id %d stopped.\n.", w.Id)
+
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
+
+func NewDispacher(jobQueue chan Job, maxWorkers int) *Dispatcher {
+	return &Dispatcher{
+		JobQueue:   jobQueue,
+		MaxWorkers: maxWorkers,
+		WorkerPool: make(chan chan Job, maxWorkers),
+	}
+}
+
+func (d *Dispatcher) Dispatch() {
+	for {
+		select {
+		case job := <-d.JobQueue:
+			go func() {
+				workerJobQueue := <-d.WorkerPool
+				workerJobQueue <- job
+			}()
+		}
+	}
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.MaxWorkers; i++ {
+		worker := NewWorker(i, d.WorkerPool)
+		worker.Start()
+	}
+	go d.Dispatch()
 }
 
 func main() {
-
-	response, err := http.Get("https://google.com/")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w := Writer{}
-	io.Copy(w, response.Body)
+	const (
+		maxWorkers   = 4
+		maxQueueSize = 20
+		port         = ":8081"
+	)
+	jobQueue := make(chan Job, maxQueueSize)
+	dispatcher := NewDispacher(jobQueue, maxWorkers)
+	dispatcher.Run()
 
 	start := time.Now()
 	servers := []string{
@@ -34,19 +106,33 @@ func main() {
 	}
 
 	for _, server := range servers {
-		checkServer(server)
+		job := Job{
+			Name:   server,
+			Server: server,
+		}
+		jobQueue <- job
 	}
 
 	timeTaken := time.Since(start)
 	fmt.Printf("exec time %s\n", timeTaken)
+	log.Fatal(http.ListenAndServe(port, nil))
 
 }
 
-func checkServer(server string) {
-	_, err := http.Get(server)
+type Writer struct{}
+
+func (Writer) Write(p []byte) (int, error) {
+	fmt.Println(string(p))
+	return len(p), nil
+}
+
+func checkServer(server string) string {
+	response, err := http.Get(server)
 	if err != nil {
-		fmt.Println(server, " is not working")
+		w := Writer{}
+		io.Copy(w, response.Body)
+		return server + " is not working"
 	} else {
-		fmt.Println(server, " is working correctly")
+		return server + " is working correctly"
 	}
 }
